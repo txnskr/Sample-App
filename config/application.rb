32bc56aae624a7018d4ff88425edb95068ec13a0
Clay Kramer <ckramer@gmail.com>
2012-05-23 17:58:33 -0700

    precompiled and added config.assets.initialize_on_precompile=false

--- config/application.rb
+++ config/application.rb
@@ -47,6 +47,9 @@ module SampleApp
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    
+    #from Heroku dev center to https://devcenter.heroku.com/articles/rails3x-asset-pipeline-cedar
+    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
